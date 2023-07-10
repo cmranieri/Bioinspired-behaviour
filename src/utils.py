925ec744bed5012d71d06f4a0a2b984b21ad2b3f
Caetano Mazzoni Ranieri <cmranieri2@gmail.com>
2023-07-10 08:53:52 -0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,14 +8,13 @@ def compute_mfr( all_spikes, t_sim, bins=50, step=None ):
     regions = dict()
     # Process each region separately
     for key in sorted( all_spikes.keys() ):
-        mfr_region = list()
-        # Spike times of each electrode within the given region
+        # Spike times of each neuron within the given region
         spikes_list = [ spk.times for spk in all_spikes[ key ] ]
+        # Sorted list of spike trains over all neurons in the region
         spikes = sorted( sum(spikes_list, [] ) )
         spikes = np.array( spikes, dtype=np.float32 )
         mfr = list()
         for t_start in range( 0, t_sim-bins+1, step ):
-        #for t_start in range( t_sim - bins ):
             mfr.append( mean_firing_rate( spikes,
                                           t_start = t_start,
                                           t_stop  = t_start + bins ) )
